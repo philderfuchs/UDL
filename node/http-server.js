f475067234a4e6ca19a21fbf0dc51eb1c05da0ed
Philipp Anders <philipp.anders.pa@gmail.com>
2017-04-07 00:01:17 +0200

    - Extracted AddEvents Method

--- node/http-server.js
+++ node/http-server.js
@@ -5,6 +5,39 @@ var key = "AIzaSyAyG8cJFWOgaWRD83UWMs_awMbvMNZSr8w";
 
 var endOfYear = 1514761200000;
 
+function addEvents(parsedData, events, category) {
+    parsedData.items.forEach(function (value) {
+        if (value.start) {
+            let startTime = new Date(value.start.dateTime.split(' ').join('T')).getTime();
+            let endTime = new Date(value.end.dateTime.split(' ').join('T')).getTime();
+            events.push({
+                title: value.summary,
+                start: startTime,
+                end: endTime,
+                class: category,
+                id: value.id,
+                url: "http://example.com"
+            });
+
+            if (value.recurrence) {
+                if (value.recurrence[0].includes("FREQ=WEEKLY")) {
+                    while (endTime < endOfYear) {
+                        startTime += 604800000;
+                        endTime += 604800000;
+                        events.push({
+                            title: value.summary,
+                            start: startTime,
+                            end: endTime,
+                            class: category,
+                            id: value.id,
+                            url: "http://example.com"
+                        });
+                    }
+                }
+            }
+        }
+    });
+}
 http.createServer(function (req, res) {
 
     https.get('https://www.googleapis.com/calendar/v3/calendars/stylewildle%40gmail.com/events?key=' + key, function (incoming) {
@@ -14,46 +47,14 @@ http.createServer(function (req, res) {
             rawData += chunk;
         });
 
-
         incoming.on('end', function () {
 
             console.log(rawData);
 
 
             let parsedData = JSON.parse(rawData);
-            var id = 0;
             let events = [];
-            parsedData.items.forEach(function (value) {
-                if (value.start) {
-                    let startTime = new Date(value.start.dateTime.split(' ').join('T')).getTime();
-                    let endTime = new Date(value.end.dateTime.split(' ').join('T')).getTime();
-                    events.push({
-                        title: value.summary,
-                        start: startTime,
-                        end: endTime,
-                        class: "event-important",
-                        id: id++,
-                        url: "http://example.com"
-                    });
-
-                    if(value.recurrence) {
-                        if(value.recurrence[0].includes("FREQ=WEEKLY")) {
-                            while(endTime < endOfYear) {
-                                startTime += 604800000;
-                                endTime += 604800000;
-                                events.push({
-                                    title: value.summary,
-                                    start: startTime,
-                                    end: endTime,
-                                    class: "event-important",
-                                    id: id++,
-                                    url: "http://example.com"
-                                });
-                            }
-                        }
-                    }
-                }
-            });
+            addEvents(parsedData, events, "event-important");
 
             let responseJson = {
                 success: 1,
