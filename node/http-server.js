f1b5ac454157879d1b648c7cfb2b27b97a389e92
Philipp Anders <philipp.anders.pa@gmail.com>
2017-04-06 16:24:14 +0200

    Answer with Google Calendar Json

--- node/http-server.js
+++ node/http-server.js
@@ -5,17 +5,26 @@ var key = "AIzaSyAyG8cJFWOgaWRD83UWMs_awMbvMNZSr8w";
 
 http.createServer(function (req, res) {
 
-    https.get('https://www.googleapis.com/calendar/v3/calendars/stylewildle%40gmail.com/events?key=' + key, function (res) {
-        res.on('data', function(d) {
-            process.stdout.write(d);
+    https.get('https://www.googleapis.com/calendar/v3/calendars/stylewildle%40gmail.com/events?key=' + key, function (incoming) {
+        let rawData = '';
+
+        incoming.on('data', function (chunk) {
+            console.log("data");
+            rawData += chunk;
+        });
+
+        incoming.on('end', function () {
+            try {
+                res.writeHead(200, {
+                    'conten-type': 'text/plain'
+                });
+                res.write(rawData);
+                res.end();
+            } catch (e) {
+                console.log(e.message);
+            }
         });
     });
 
-    res.writeHead(200, {
-        'conten-type': 'text/plain'
-    });
-    res.write('Ciao\n');
-    setTimeout(function() {
-        res.end('Welt!\n');
-    }, 2000)
+
 }).listen('3000');
