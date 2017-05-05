1d8028978e94cd24e3690a9b78f63ac0ed37f680
Philipp Anders <philipp.anders.pa@gmail.com>
2017-05-05 23:25:07 +0200

    - async handling by async package

--- node/http-server.js
+++ node/http-server.js
@@ -2,6 +2,7 @@ var http = require('http');
 var https = require('https');
 var request = require('request');
 var rp = require('request-promise');
+var async = require('async');
 
 var key = "AIzaSyDkdDAWXsoECm144VHaKEwtkpcUhSKWPXA";
 
@@ -49,100 +50,107 @@ function addEvents(parsedData, events, category) {
     });
 }
 
-async function doRequests(res) {
-    let events = [];
-
-    // HIPHOP
-    await rp('https://www.googleapis.com/calendar/v3/calendars/h86f19mfhajijbqr4fld0ghalk@group.calendar.google.com/events?key=' + key)
-        .then(function (body) {
-            addEvents(JSON.parse(body), events, "event-standard");
-        })
-        .catch(function (err) {
-            console.log(">>>>>>>> ERROR GETTING HIPHOP CALENDAR");
-        });
-
-    // BREAKDANCE
-    await rp('https://www.googleapis.com/calendar/v3/calendars/ole9uvl71kba7sr2jid3f1nsgk@group.calendar.google.com/events?key=' + key)
-        .then(function (body) {
-            addEvents(JSON.parse(body), events, "event-important");
-        })
-        .catch(function (err) {
-            console.log(">>>>>>>> ERROR GETTING BREAKDANCE CALENDAR");
-        });
-
-    // WORKSHOPS, PARTIES, JAMS
-    await rp('https://www.googleapis.com/calendar/v3/calendars/tlu070aums1aegpgbsake8u334%40group.calendar.google.com/events?key=' + key)
-        .then(function (body) {
-            // console.log(body);
-            addEvents(JSON.parse(body), events, "event-success");
-        })
-        .catch(function (err) {
-            console.log(">>>>>>>> ERROR GETTING WORKSHOPS CALENDAR");
-        });
-
-    // HOUSE
-    await rp('https://www.googleapis.com/calendar/v3/calendars/s49l2gua3gai6rs9imfvtnf9qo%40group.calendar.google.com/events?key=' + key)
-        .then(function (body) {
-            // console.log(body);
-            addEvents(JSON.parse(body), events, "event-warning");
-        })
-        .catch(function (err) {
-            console.log(">>>>>>>> ERROR GETTING HOUSE CALENDAR");
-            // console.log(err);
-        });
-
-    // LOCKING / POPPING
-    await rp('https://www.googleapis.com/calendar/v3/calendars/o5gf1c9ila9jj21gkvipnposoo@group.calendar.google.com/events?key=' + key)
-        .then(function (body) {
-            // console.log(body);
-            addEvents(JSON.parse(body), events, "event-info");
-        })
-        .catch(function (err) {
-            console.log(">>>>>>>> ERROR GETTING LOCKING/POPPING CALENDAR");
-            // console.log(err);
-        });
-
-    // Other Styles
-    await rp('https://www.googleapis.com/calendar/v3/calendars/n339bp32ism75mr3qfms6jrdfk@group.calendar.google.com/events?key=' + key)
-        .then(function (body) {
-            // console.log(body);
-            addEvents(JSON.parse(body), events, "event-inverse");
-        })
-        .catch(function (err) {
-            console.log(">>>>>>>> ERROR GETTING OTHER STYLES CALENDAR");
-            // console.log(err);
-        });
-
-    // ALL STYLES TRAININGS
-    await rp('https://www.googleapis.com/calendar/v3/calendars/77falh44dpf9mj35iakun2pt84@group.calendar.google.com/events?key=' + key)
-        .then(function (body) {
-            addEvents(JSON.parse(body), events, "event-special");
-        })
-        .catch(function (err) {
-            console.log(">>>>>>>> ERROR GETTING ALL STYLES TRAININGS CALENDAR");
-        });
-
-    let responseJson = {
-        success: 1,
-        result: events.filter(function (n) {
-            return n != undefined
-        })
-    };
-    try {
-        res.writeHead(200, {
-            'conten-type': 'application/json',
-            'Access-Control-Allow-Origin': "*"
-        });
-        res.write(JSON.stringify(responseJson));
-        res.end();
-    } catch (e) {
-        console.log(e.message);
-    }
-}
-
 
 http.createServer(function (req, res) {
 
-    doRequests(res);
+    let events = [];
+
+    // HIPHOP
+    async.parallel([
+        function (callback) {
+            rp('https://www.googleapis.com/calendar/v3/calendars/h86f19mfhajijbqr4fld0ghalk@group.calendar.google.com/events?key=' + key)
+                .then(function (body) {
+                    addEvents(JSON.parse(body), events, "event-standard");
+                    callback();
+                })
+                .catch(function (err) {
+                    console.log(">>>>>>>> ERROR GETTING HIPHOP CALENDAR");
+                });
+        }, function (callback) {
+
+            // BREAKDANCE
+            rp('https://www.googleapis.com/calendar/v3/calendars/ole9uvl71kba7sr2jid3f1nsgk@group.calendar.google.com/events?key=' + key)
+                .then(function (body) {
+                    addEvents(JSON.parse(body), events, "event-important");
+                    callback();
+                })
+                .catch(function (err) {
+                    console.log(">>>>>>>> ERROR GETTING BREAKDANCE CALENDAR");
+                })
+        }, function (callback) {
+            // WORKSHOPS, PARTIES, JAMS
+            rp('https://www.googleapis.com/calendar/v3/calendars/tlu070aums1aegpgbsake8u334%40group.calendar.google.com/events?key=' + key)
+                .then(function (body) {
+                    addEvents(JSON.parse(body), events, "event-success");
+                    callback();
+                })
+                .catch(function (err) {
+                    console.log(">>>>>>>> ERROR GETTING WORKSHOPS CALENDAR");
+                });
+        }, function (callback) {
+            // HOUSE
+            rp('https://www.googleapis.com/calendar/v3/calendars/s49l2gua3gai6rs9imfvtnf9qo%40group.calendar.google.com/events?key=' + key)
+                .then(function (body) {
+                    addEvents(JSON.parse(body), events, "event-warning");
+                    callback();
+                })
+                .catch(function (err) {
+                    console.log(">>>>>>>> ERROR GETTING HOUSE CALENDAR");
+                    // console.log(err);
+                });
+        }, function (callback) {
+            // LOCKING / POPPING
+            rp('https://www.googleapis.com/calendar/v3/calendars/o5gf1c9ila9jj21gkvipnposoo@group.calendar.google.com/events?key=' + key)
+                .then(function (body) {
+                    addEvents(JSON.parse(body), events, "event-info");
+                    callback();
+                })
+                .catch(function (err) {
+                    console.log(">>>>>>>> ERROR GETTING LOCKING/POPPING CALENDAR");
+                    // console.log(err);
+                });
+        }, function (callback) {
+            // Other Styles
+            rp('https://www.googleapis.com/calendar/v3/calendars/n339bp32ism75mr3qfms6jrdfk@group.calendar.google.com/events?key=' + key)
+                .then(function (body) {
+                    addEvents(JSON.parse(body), events, "event-inverse");
+                    callback();
+                })
+                .catch(function (err) {
+                    console.log(">>>>>>>> ERROR GETTING OTHER STYLES CALENDAR");
+                    // console.log(err);
+                });
+        }, function (callback) {
+            // ALL STYLES TRAININGS
+            rp('https://www.googleapis.com/calendar/v3/calendars/77falh44dpf9mj35iakun2pt84@group.calendar.google.com/events?key=' + key)
+                .then(function (body) {
+                    addEvents(JSON.parse(body), events, "event-special");
+                    callback();
+                })
+                .catch(function (err) {
+                    console.log(">>>>>>>> ERROR GETTING ALL STYLES TRAININGS CALENDAR");
+                });
+        }
+    ], function (err, results) {
+
+        let responseJson = {
+            success: 1,
+            result: events.filter(function (n) {
+                return n != undefined
+            })
+        };
+
+        try {
+            res.writeHead(200, {
+                'conten-type': 'application/json',
+                'Access-Control-Allow-Origin': "*"
+            });
+            res.write(JSON.stringify(responseJson));
+            res.end();
+        } catch (e) {
+            console.log(e.message);
+        }
+    });
+
 
 }).listen('8080');
