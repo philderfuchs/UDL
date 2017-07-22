bc511f0d6628bc31efdf23af9def2007c100bbd1
Philipp Anders <philipp.anders.pa@gmail.com>
2017-07-22 20:08:42 +0200

    Article preview

--- js/blog/components/ArticlePreview.js
+++ js/blog/components/ArticlePreview.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-
+import {IntlProvider, FormattedRelative} from 'react-intl';
 
 export default class ArticlePreview extends React.Component {
 
@@ -11,12 +11,40 @@ export default class ArticlePreview extends React.Component {
     }
 
     render() {
-        const link = "/article/" + this.article.id;
+        const headerImageStyle = {
+            backgroundImage: "url(" + this.article.imageurl + ")"
+        };
         return (
-            <div class="col-xs-12">
-                <h1>{this.article.title}</h1>
-                <span>{this.article.short}</span>
-                <Link to={link}> mehr </Link>
+            <div class="col-xs-12 preview">
+                <div class="headerImage" style={headerImageStyle}/>
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 col-xs-12">
+                        <h3 class="title">
+                            {this.article.title}<br/>
+                            <IntlProvider locale="en">
+                                <small><i class="fa fa-clock-o"/> <FormattedRelative value={this.article.date}/></small>
+                            </IntlProvider>
+                            <small><a href={"mailto:" + this.article.email}> <i class="fa fa-user-circle-o"/> {this.article.author}</a>
+                            </small>
+                            <small><a href={this.article.url}> <i class="fa fa-external-link"/> homepage</a></small>
+                        </h3>
+                        <div class="colorstripe-container">
+                            <div class="colorstripe">
+                                <div class="colorblock event-standard"></div>
+                                <div class="colorblock event-important"></div>
+                                <div class="colorblock event-warning"></div>
+                                <div class="colorblock event-info"></div>
+                                <div class="colorblock event-inverse"></div>
+                                <div class="colorblock event-success"></div>
+                                <div class="colorblock event-special"></div>
+                            </div>
+                        </div>
+                        <div class="text">
+                            {this.article.text_preview}
+                            <Link to={"/article/" + this.article.id}>... mehr lesen </Link>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
--- js/blog/index.js
+++ js/blog/index.js
@@ -2,26 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {HashRouter as Router, Switch, Route, Link} from 'react-router-dom'
 
-import Articles from './resources/articles';
 
 import Overview from './components/Overview';
 import FullArticle from './components/FullArticle';
 
 $(() => {
+
+    $.get("https://udl.cloudno.de/articles", function(articles) {
+
         ReactDOM.render(
             <Router>
                 <div class="row">
-                    <div class="col-xs-12">
-                        <h1 class="caption"> Blog </h1>
+                    <div class="col-xs-12 caption">
+                        <h1> Blog </h1>
                     </div>
                     <Switch>
-                        <Route exact path="/" component={(props) => <Overview {...props} articles={Articles}/>}/>
+                        <Route exact path="/" component={(props) => <Overview {...props} articles={articles}/>}/>
                         <Route exact path="/article/:article"
-                               component={(props) => <FullArticle {...props} articles={Articles}/>}/>
+                               component={(props) => <FullArticle {...props} articles={articles}/>}/>
                     </Switch>
                 </div>
             </Router>,
 
             $('#blogcontainer')[0]);
+    })
+
+
     }
 );
